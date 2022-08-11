d2016113b3f6769b4444256fe79ff5493bc08363
@eduardoLima <eduardolimacesl@gmail.com>
2022-08-11 15:31:37 -0300

    feat: implemanting loader functions

--- script.js
+++ script.js
@@ -3,8 +3,20 @@ const quoteText = document.getElementById('quote');
 const quoteAuthor = document.getElementById('author');
 const twitterBtn = document.getElementById('twitter-btn');
 const newQuoteBtn = document.getElementById('newQuote-btn');
+const loader = document.getElementById('loader');
+
+function loading() {
+  loader.hidden = false;
+  quoteContainer.hidden = true;
+}
+
+function complete() {
+  quoteContainer.hidden = false;
+  loader.hidden = true;
+}
 
 function newQuote(arrayOfQuotes) {
+  loading();
   const randomNumber = Math.floor(Math.random() * arrayOfQuotes.length)
   const newQuote = arrayOfQuotes[randomNumber]
   if (!newQuote.author) {
@@ -18,10 +30,12 @@ function newQuote(arrayOfQuotes) {
     quoteText.classList.remove('long-quote')
   }
   quoteText.textContent = newQuote.text;
+  complete();
 }
 
 // Get quotes from API
 async function getQuotes () {
+  loading()
   const urlAPI = 'https://type.fit/api/quotes'
   try {
     const arrayOfQuotes = await fetch(urlAPI)
@@ -32,6 +46,17 @@ async function getQuotes () {
   }
 }
 
-// On load
-const quote = getQuotes();
+//twitte button
+function sendTwitter() {
+  const twitterUrl = `https://twitter.com/intent/tweet?text=${quoteText.textContent} - ${quoteAuthor.textContent}`
+  window.open(twitterUrl, '_blanck')
+}
 
+//Event listners
+newQuoteBtn.addEventListener('click', getQuotes)
+twitterBtn.addEventListener('click', sendTwitter)
+
+// On load
+getQuotes();
+// loading()
+complete()
